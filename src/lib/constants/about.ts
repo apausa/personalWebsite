22c42f9c28ffa3b03062535c203e83683504573a
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-09-27 12:47:01 +0200

    Add project description constant

--- src/lib/constants/about.ts
+++ src/lib/constants/about.ts
@@ -7,6 +7,14 @@ export const description = [
   "I learned programming by creative coding during high school, then built web and mobile apps while studying design at university. This led me to pursue computer science to deepen my understanding of theoretical foundations. I now specialize in deep learning, neural networks, and their applications. With this interdisciplinary background spanning human-computer interaction to software engineering, I approach challenges with a holistic perspective.",
 ];
 
+export const projects = [
+  {
+    name: "Personal website",
+    description:
+      "Full Stack Web Application developed with Next.js and TypeScript. Fetching data from the Spotify API, and featuring generative art using p5.js.",
+  },
+];
+
 export const links = [
   {
     name: "LinkedIn",
