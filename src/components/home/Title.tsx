71a9f914aff4861cfd784bde9085649e4a458797
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2026-01-23 19:04:54 +0100

    Update title

--- src/components/home/Title.tsx
+++ src/components/home/Title.tsx
@@ -5,11 +5,7 @@ import { cn } from "@/lib/utils/tailwind";
 export default function Title({ className }: { className: string }) {
   return (
     <section className={cn("flex flex-col gap-2", className)}>
-      <h1 className="text-4xl font-bold">Pablo Apausa</h1>
-      <h2 className="text-xl">
-        Software Engineer at CERN Future Circular Collider Study — MSc in
-        Artificial Intelligence at Technical University of Madrid
-      </h2>
+      <h1 className="text-5xl font-medium">Pablo Apausa</h1>
     </section>
   );
 }
