4018f46a4a56520df7393439bb3d1a38eed7518c
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2026-01-23 19:04:13 +0100

    Update about

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -3,8 +3,7 @@ import { cn } from "@/lib/utils/tailwind";
 
 export default function About({ className }: { className: string }) {
   return (
-    <section className={cn("flex flex-col gap-2 pb-2", className)}>
-      <h3 className="font-bold">About me</h3>
+    <section className={cn("flex flex-col gap-4 pb-2 text-justify", className)}>
       {description.map((paragraph) => (
         <p key={paragraph}>{paragraph}</p>
       ))}
