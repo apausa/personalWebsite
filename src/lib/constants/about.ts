a0d1c71711469a22509e60f825da1c139cb0f9d6
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2026-03-31 21:59:59 +0200

    Remove constants

--- src/lib/constants/about.ts
+++ src/lib/constants/about.ts
@@ -3,20 +3,6 @@ import {
   IconBrandLinkedinFilled,
 } from "@tabler/icons-react";
 
-export const description = [
-  "I taught myself to code by making generative art in high school, then built web applications while studying digital design at university. This led me to computer science, where I got hooked on neural networks and deep learning.",
-  "Now I research computer vision at the Technical University of Madrid. This background from human-computer interaction to artificial intelligence lets me tackle problems from different perspectives.",
-];
-
-export const projects = [
-  {
-    name: "Personal Website",
-    href: "https://github.com/apausa/personalWebsite",
-    description:
-      "Full Stack Web Application developed with Next.js and TypeScript. Fetching data from the Spotify API, and featuring generative art using p5.js.",
-  },
-];
-
 export const links = [
   {
     name: "LinkedIn",
