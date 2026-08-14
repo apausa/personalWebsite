1737535be9b92df77bb9acf8ee96f498391ca72c
pablo <74920898+apausa@users.noreply.github.com>
2026-08-14 23:04:55 +0200

    Update about me component

--- src/components/page/about.tsx
+++ src/components/page/about.tsx
@@ -7,9 +7,9 @@ export default function Highlights() {
   return (
     <Card className={cn("gap-2 bg-0")}>
       <CardHeader className="font-bold">
-        <CardTitle>About me</CardTitle>
+        <CardTitle className="font-serif text-lg font-bold">About me</CardTitle>
       </CardHeader>
-      <CardContent className="flex flex-col gap-4">
+      <CardContent className="flex flex-col gap-2 text-lg">
         {ABOUT.map((paragraph) => (
           <p key={paragraph}>{paragraph}</p>
         ))}
