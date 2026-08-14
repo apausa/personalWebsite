52cf49ced1fc7849757279c7ecb8133d0ec4c840
pablo <74920898+apausa@users.noreply.github.com>
2026-08-14 23:04:15 +0200

    Update constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 import {
+  IconBookmark,
   IconBrandGithubFilled,
   IconBrandHackerrank,
   IconBrandLinkedinFilled,
@@ -7,7 +8,7 @@ import {
 export const TITLE = "Pablo Apausa";
 
 export const DESCRIPTION =
-  "Software Engineer at CERN — MS in Artificial Intelligence";
+  "Software Engineer at CERN FCC Study — MS in Artificial Intelligence";
 
 export const METADATA = {
   title: TITLE,
@@ -16,7 +17,6 @@ export const METADATA = {
 
 export const ABOUT = [
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.",
-  'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
 ];
 
 export const LINKS = [
