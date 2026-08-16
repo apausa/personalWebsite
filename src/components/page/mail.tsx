6ea39cc5192755cbed675e1ae509f7ae364b4a9d
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2026-08-16 13:19:50 +0200

    Create mail component

---
+++ src/components/page/mail.tsx
@@ -0,0 +1,19 @@
+import { IconMailFilled } from "@tabler/icons-react";
+
+import { cn } from "@/lib/utils";
+
+export function Mail() {
+  return (
+    <p className={cn("min-w-0 break-words")}>
+      <IconMailFilled className="w-4 h-4 inline-block mr-2" />
+      <a
+        className="hover:underline dark:text-custom-green"
+        href="mailto:pablo@apausa.dev"
+        rel="noreferrer"
+        target="_blank"
+      >
+        pablo@apausa.dev
+      </a>
+    </p>
+  );
+}
